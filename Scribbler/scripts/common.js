10ca0c8cb5b6a75a1e54fdddc2ab70c5ea85ee58
Anil Narayanan <narayanan.anil@gmail.com>
2020-06-05 00:23:12 +0530

    Implementing Part-B

--- Scribbler/scripts/common.js
+++ Scribbler/scripts/common.js
@@ -1,23 +1,23 @@
 function signUp() {
   var template =
     "<div id='segment2-child' class='segment-2-child-div'>\
-    <form><p class='segment-2-title'><label>Get Started</label><span class='segment-2-close'><i class='fa fa-times' aria-hidden='true' onclick='closeSegment2Parent()'></i></span></p>\
+    <form><p class='title-prop'><label>Get Started</label><span class='close-win-prop'><i class='fa fa-times' aria-hidden='true' onclick='closeSegment2Parent()'></i></span></p>\
           <hr>\
-          <p class='segment-2-p'><label class='segment-2-label'>Name</label></p>\
+          <p class='segment-2-p'><label class='label-prop'>Name</label></p>\
           <p class='segment-2-p'>\
-              <input type='text' id='input-name' class='segment-2-input' placeholder='Enter your name' onblur='inputValidation(this)' required'></input>\
+              <input type='text' id='input-name' class='input-text-prop input-text-prop-width' placeholder='Enter your name' onblur='inputValidation(this)' required'></input>\
           </p>\
-          <p class='segment-2-p'><label class='segment-2-label'>Username</label></p>\
+          <p class='segment-2-p'><label class='label-prop'>Username</label></p>\
           <p class='segment-2-p'>\
-              <input type='text' id='input-username' class='segment-2-input' placeholder='Enter your username'></input>\
+              <input type='text' id='input-username' class='input-text-prop input-text-prop-width' placeholder='Enter your username'></input>\
           </p>\
-          <p class='segment-2-p'><label class='segment-2-label'>Password</label></p>\
+          <p class='segment-2-p'><label class='label-prop'>Password</label></p>\
           <p class='segment-2-p'>\
-              <input type='password' id='input-pwd' class='segment-2-input' placeholder='Enter your password'></input>\
+              <input type='password' id='input-pwd' class='input-text-prop input-text-prop-width' placeholder='Enter your password'></input>\
           </p>\
-          <p class='segment-2-p'><label class='segment-2-label'>Confirm Password</label></p>\
+          <p class='segment-2-p'><label class='label-prop'>Confirm Password</label></p>\
           <p class='segment-2-p'>\
-              <input type='password' id='input-confirmpwd' class='segment-2-input' placeholder='Re-enter your password'></input>\
+              <input type='password' id='input-confirmpwd' class='input-text-prop input-text-prop-width' placeholder='Re-enter your password'></input>\
           </p>\
           </form><p class='segment-2-p'><button class='segment-2-btn' onclick='signUpUser(input-name, input-username, input-pwd, input-confirmpwd)'>Sign Up</button></p>\
           </div>";
@@ -40,18 +40,40 @@ function closeSegment2Parent() {
 function signIn() {
   var template =
     "<div id='segment2-child' class='segment-2-child-div'>\
-    <form><p class='segment-2-title'><label>Welcome Back!</label><span class='segment-2-close'><i class='fa fa-times' aria-hidden='true' onclick='closeSegment2Parent()'></i></span></p>\
+    <form><p class='title-prop'><label>Welcome Back!</label><span class='close-win-prop'><i class='fa fa-times' aria-hidden='true' onclick='closeSegment2Parent()'></i></span></p>\
           <hr>\
-          <p class='segment-2-p'><label class='segment-2-label'>Username</label></p>\
+          <p class='segment-2-p'><label class='label-prop'>Username</label></p>\
           <p class='segment-2-p'>\
-              <input type='text' id='input-username' class='segment-2-input' placeholder='Enter your username'></input>\
+              <input type='text' id='input-username' class='input-text-prop input-text-prop-width' placeholder='Enter your username'></input>\
           </p>\
-          <p class='segment-2-p'><label class='segment-2-label'>Password</label></p>\
+          <p class='segment-2-p'><label class='label-prop'>Password</label></p>\
           <p class='segment-2-p'>\
-              <input type='password' id='input-pwd' class='segment-2-input' placeholder='Enter your password'></input>\
+              <input type='password' id='input-pwd' class='input-text-prop input-text-prop-width' placeholder='Enter your password'></input>\
           </p>\
           </form><p class='segment-2-p'><button class='segment-2-btn' onclick='signUpUser(input-name, input-username, input-pwd, input-confirmpwd)'>Sign In</button></p>\
           <p class='segment-2-p2'>Not a member? <a href='#' onclick = 'closeSegment2Parent();signUp()'>Sign Up</a></p>\
           </div>";
   document.getElementById("segment2").innerHTML = template;
+}
+
+function createPost() {
+  var template =
+    "<div id='segment2-child' class='segment-2-child-div'>\
+    <form><p class='title-prop'><label>Pen Your Post</label><span class='close-win-prop'><i class='fa fa-times' aria-hidden='true' onclick='closeSegment2Parent()'></i></span></p>\
+          <hr>\
+          <p class='segment-2-p'><label class='label-prop'>Title</label></p>\
+          <p class='segment-2-p'>\
+              <input type='text' id='input-username' class='input-text-prop input-text-prop-width' placeholder='Enter title of your post'></input>\
+          </p>\
+          <p class='segment-2-p'><label class='label-prop'>Contents</label></p>\
+          <p class='segment-2-p'>\
+              <textarea class='input-text-prop input-textarea-prop' rows='20' cols='120' placeholder='Enter the contents of your post'></textarea>\
+          </p>\
+          </form><p class='segment-2-p'><button class='segment-2-btn' onclick='' style=''>Create</button></p>\
+          </div>";
+  document.getElementById("segment2").innerHTML = template;
+}
+
+function redirectAllPostsPage() {
+  window.open("./html/postslist.html", "_self");
 }
