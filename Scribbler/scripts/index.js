b5443d44bccd5dd20a41a82aee8381020efa55bc
Anil Narayanan <narayanan.anil@gmail.com>
2020-06-08 23:34:06 +0530

    Updated code

---
+++ Scribbler/scripts/index.js
@@ -0,0 +1,22 @@
+function createPost() {
+  var template =
+    "<div id='segment2-modal' class='segment-2-child-modal'>\
+          <form><div><p class='title-prop'><label>Pen Your Post</label><span class='close-win-prop'><i class='fa fa-times' aria-hidden='true' onclick='closeSegment2Parent()'></i></span></p>\
+            <hr>\
+            <p class='segment-2-p'><label class='label-prop'>Title</label></p>\
+            <p class='segment-2-p'>\
+                <input type='text' id='input-username' class='input-text-prop input-text-prop-width' placeholder='Enter title of your post' required></input>\
+            </p>\
+            <p class='segment-2-p'><label class='label-prop'>Contents</label></p>\
+            <p class='segment-2-p'>\
+                <textarea class='input-text-prop input-text-prop-width' rows='20' cols='120' placeholder='Enter the contents of your post' required></textarea>\
+            </p>\
+            <span class='test'><button class='createPostBtn' onclick='' style=''>Create</button></span>\
+        </div></form>\
+      </div>";
+  document.getElementById("segment2").innerHTML = template;
+}
+
+function redirectAllPostsPage() {
+  window.open("./html/postslist.html", "_self");
+}
---
+++ Scribbler/scripts/post.js
@@ -0,0 +1,61 @@
+var postcard;
+var nLikes = 0;
+
+/**
+ * On document load, load post details appropriately between the tags
+ */
+window.onload = function () {
+  var id = this.sessionStorage.getItem("id");
+  document.getElementById("author").innerHTML = this.postDetails[id].author;
+  document.getElementById("title").innerHTML = this.postDetails[id].title;
+  document.getElementById("content").innerHTML = this.postDetails[id].content;
+  document.getElementById("nLikes").innerHTML =
+    "Be the first one to like this!";
+};
+
+/**
+ * Method to edit Title and Contents of a post
+ */
+function edit() {
+  document.getElementById("edit").innerHTML =
+    "Save <i id='save' class='fa fa-save'></i>";
+  document.getElementById("title").setAttribute("contentEditable", "true");
+  document.getElementById("title").style.border = "thin solid red";
+  document.getElementById("content").setAttribute("contentEditable", "true");
+  document.getElementById("content").style.border = "thin solid red";
+  document.getElementById("edit").setAttribute("id", "save");
+  document.getElementById("save").setAttribute("onclick", "save()");
+}
+
+/**
+ * Method to save changes applied in the Title and Contents elements
+ */
+function save() {
+  document.getElementById("title").setAttribute("contentEditable", "false");
+  document.getElementById("title").style.border = "0";
+  document.getElementById("content").setAttribute("contentEditable", "false");
+  document.getElementById("content").style.border = "0";
+  document.getElementById("save").setAttribute("id", "edit");
+  document.getElementById("edit").setAttribute("onclick", "edit()");
+  document.getElementById("edit").innerHTML =
+    "Edit <i id='edit' class='fa fa-edit'></i>";
+}
+
+/**
+ * Increments number of Likes
+ */
+function like() {
+  nLikes += 1;
+  document.getElementById("nLikes").innerHTML = nLikes + " person likes this!";
+  document.getElementById("like-button").innerHTML =
+    "<i class='fa fa-thumbs-up'></i> Liked";
+}
+
+/**
+ * Method to add comments to the post
+ */
+function addComment(comment) {
+  console.log(comment.value);
+  document.querySelector(".div-comment").style.display = "flex";
+  document.getElementById("divComment").prepend();
+}
